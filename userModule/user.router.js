369fc842040b8dc04d69445ef0b3da42f333bc24
Khmeliuk Nazarii <nazik.klas.opaopaopapa@gmail.com>
2023-09-14 12:45:16 +0300

    s

--- userModule/user.router.js
+++ userModule/user.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userController from "./controller/user.controller.js";
 import { body } from "express-validator";
-import authMiddleware from "../middleware/auth-middleware.js";
+import authMiddleware, {
+  isActivatedMiddleware
+} from "../middleware/auth-middleware.js";
 const userRouter = new Router();
 
 userRouter.post(
@@ -14,5 +16,10 @@ userRouter.post("/login", userController.loginUser);
 userRouter.get("/activate/:link", userController.verifyEmail);
 userRouter.post("/logout", userController.logOut);
 userRouter.post("/refresh", userController.refresh);
-userRouter.post("/authorize", authMiddleware, userController.authorize);
+userRouter.post(
+  "/authorize",
+  authMiddleware,
+  isActivatedMiddleware,
+  userController.authorize
+);
 export default userRouter;
